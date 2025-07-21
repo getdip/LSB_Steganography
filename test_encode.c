de79608d5fd31679535e78f9826a06f671cf4adf
Dip Chakraborty <70464668+getdip@users.noreply.github.com>
2025-07-21 19:24:00 +0530

    Update test_encode.c

--- test_encode.c
+++ test_encode.c
@@ -6,49 +6,32 @@
 #include <unistd.h>
 
 
-
-
-
-
-
 int main(int argc, char* argv[])
 {
-   
    //check operation type -e or -d
 
    EncodeInfo infoptr;
 
-   if   (check_operation_type(argv) == e_encode)                        /// if ENCODING is selected 
+   if   (check_operation_type(argv) == e_encode)                        // if ENCODING is selected 
    
    {
-
         printf("Selected Encoding\n");                                              
 
-        if(read_and_validate_encode_args(argv,&infoptr) == e_success )       /// validate the CLA vector i.e the .bmp files and .txt files
+        if(read_and_validate_encode_args(argv,&infoptr) == e_success )       // validate the CLA inputs i.e the .bmp files and .txt files
         
     {         
-
-        
-
         printf("Read and validate argument is successfull\n");
 
             if(do_encoding(&infoptr) == e_success)
         {
         
             printf("Completed Encoding\n");
-
-            
             FILE *enc_chck;
             enc_chck=fopen("enc_completion_check.txt","w+");
             fputs(infoptr.stego_image_fname, enc_chck);
-
-            
-
-
         }
 
     }
-
         else if (read_and_validate_encode_args(argv,&infoptr) == e_failure)
         {
 
@@ -57,10 +40,7 @@ int main(int argc, char* argv[])
 
    }
 
-
-   
-
-  else if  (check_operation_type(argv) == e_decode)
+  else if  (check_operation_type(argv) == e_decode)                         //if DECODING is selected
   
   {
 
@@ -70,67 +50,26 @@ int main(int argc, char* argv[])
         if(access("enc_completion_check.txt",F_OK) == 0){
 
             printf("Encoding has completed\n");
-
-
             printf("Now decoding will begin\n");
-
             //void loadingBar();
-
-
-
              DecodeInfo decInfo;
-
-             
-
        
-            if(read_and_validate_decode_args(argv,&decInfo) == e_success)
-
-            {
-
-                if(do_decoding(&decInfo) == e_success)
-
-                {
-
+            if(read_and_validate_decode_args(argv,&decInfo) == e_success){
+                if(do_decoding(&decInfo) == e_success){
                     printf("Decoding finished\n");
                 }
-
-           
-
-                
             }
 
-            else if(read_and_validate_decode_args(argv,&decInfo) == e_failure)
-            
-            {
-
+            else if(read_and_validate_decode_args(argv,&decInfo) == e_failure){
 
                 printf("Wrong input image file for the decoding process\n");
-
             }
+        }
 
-        
-
-        
-        
-      
-
-
-        
-
-                }
-
-            
-            else 
-
-            {
-
-                printf("Encoding is not completed\nPlease complete Encoding first and then proceed with decoding\n");
-            }
-
-
-
+        else {
+            printf("Encoding is not completed\nPlease complete Encoding first and then proceed with decoding\n");
+        }
   }
-
     return 0;
 }
 
@@ -142,13 +81,10 @@ OperationType check_operation_type(char* argv[]){
 
         return e_encode;
     }
-
     else if (strcmp(argv[1],"-d" )==0){
 
         return e_decode;
     }
-
-
     else{
 
         return e_unsupported;
@@ -156,10 +92,6 @@ OperationType check_operation_type(char* argv[]){
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-
-
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
